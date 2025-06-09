daf2e71b8d276f8e51a2a00644012075dbfb4aac
msmsm0320 <ms000320@naver.com>
2025-06-09 11:18:37 +0900

    INIT : 백엔드 프로젝트 세팅

---
+++ backend/src/test/kotlin/io/msmsm0320/backend/BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.msmsm0320.backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
